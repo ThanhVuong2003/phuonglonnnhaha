cae0c84b71246c5b5c474165237627ddd6703af4
ThanhVuong2003 <87897088+ThanhVuong2003@users.noreply.github.com>
2021-07-24 17:41:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Phương Hâm àaa!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
+    Mà sao Phương cứ đi nhầm đường :))
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnIntro: 'HEHEHEHEHE :))',
+    title: 'Phải chăng Phương đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Phương đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích không m có thichhhh taooo khong ?? Có nhée :V <33',
+    btnNo: 'Không, Anh trai à :3 - Đừng Ấn Vào Đây :(((',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Lợnn lại yêu Tó <3',
+    btnReply: 'Gửi cho Tao ngheeee <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay 7h Tao đón qua bãi tha ma nghee :V Con hưm dở hơi :))).',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: '//https://www.facebook.com/messages/t/100014845386851
 }

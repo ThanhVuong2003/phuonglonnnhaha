30f4651fc92f2a13fae9f06a79583b50141397c4
ThanhVuong2003 <87897088+ThanhVuong2003@users.noreply.github.com>
2021-07-24 17:25:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
-    introTitle: 'Lợn Hâm à :))!',
+    introTitle: ' PHƯƠNG Hâm à :))!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao phương cứ đi nhầm đường
+    Mà sao Phương cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi :3',
+    btnIntro: 'HEHEHE :3',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Vẫn cứ là thích tóóó :)) <33',
     btnNo: 'Không, Anh trai à :3 - ĐỪNG MÀ Pleaseee :((',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Dmm Gửi cho tó ngheee <3',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //https://www.facebook.com/messages/t/100014845386851
 }

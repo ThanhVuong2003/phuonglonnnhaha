5121f3492708a7f059f0ce2c022cd8f40894e1f1
ThanhVuong2003 <87897088+ThanhVuong2003@users.noreply.github.com>
2021-07-24 17:32:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'HEHEHE :3',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng Phương Hâm :V đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Phươngg DỒ :)) đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích tóóó :)) <33',
     btnNo: 'Không, Anh trai à :3 - ĐỪNG MÀ Pleaseee :((',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Dmm Gửi cho tó ngheee <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Chắc chắn đi :)) hông cưỡng được đâu ieu ngheee <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay 7h taoo đón qua bãi tha ma nhé :V công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //https://www.facebook.com/messages/t/100014845386851
 }
